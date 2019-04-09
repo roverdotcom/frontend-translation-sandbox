6ce9ceaa1b040307c35a51fa42a5afa0c223893f
An Nguyen <an@rover.com>
2019-04-09 11:02:39 -0700

    add vue

---
+++ src/app/components/App.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  .container Hello {{app}}
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  data() {
+    return {
+      app: "Vue"
+    };
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.container {
+  color: green;
+}
+</style>
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from "react-dom";
+import Vue from "vue";
 
 import { App } from './react-app/components/App'
+import VueApp from './app/components/App.vue'
 import "./styles.css";
 
 var mountNode = document.getElementById("app");
 ReactDOM.render(<App name="Jane" />, mountNode);
+new Vue(VueApp).$mount('#vue-app') ;
